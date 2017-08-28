fd63ce1ee33f190d0cb5971e269387a329be32f2
Javed Wright <jdwright870@gmail.com>
2017-08-27 23:21:53 -0500

    obtains site domain name

--- domain_finder.py
+++ domain_finder.py
@@ -6,7 +6,8 @@ def get_domain_name(url):
     try:
         results = get_sub_domain_name(url).split('.')
         return results[-2] + '.' + results[-1]
-    except:
+    except Exception as e:
+        print(str(e))
         return ''
 
 
