72eb443fc6de3f6e388620f74fdd1fb68fbf797e
Javed Wright <jdwright870@gmail.com>
2017-08-27 23:23:35 -0500

    added site-to-url domain name matching

--- link_complier.py
+++ link_complier.py
@@ -1,13 +1,14 @@
 from html.parser import HTMLParser
 from urllib import parse
+from domain_finder import *
 
 
 class LinkFinder(HTMLParser):
 
-    def __init__(self, base_url, page_url):
+    def __init__(self, base_url, domain):
         super(LinkFinder, self).__init__()
         self.base_url = base_url
-        self.page_url = page_url
+        self.domain = domain
         self.links = set()
 
     # When we call HTMLParser feed() this function is called when it encounters an opening tag <a>
@@ -16,7 +17,8 @@ class LinkFinder(HTMLParser):
             for (attribute, value) in attrs:
                 if attribute == 'href':
                     url = parse.urljoin(self.base_url, value)
-                    self.links.add(url)
+                    if get_domain_name(url) == self.domain:
+                        self.links.add(url)
 
     def page_links(self):
         return self.links
